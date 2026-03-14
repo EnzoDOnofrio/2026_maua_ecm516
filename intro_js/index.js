171e88908579f5942458ca7e2bd4ecc112249935
enzo_donofrio <enzo.oliveira2004@outlook.com>
2026-03-14 09:32:21 -0300

    learn(ap01-js): lida com codigo 3.3.1

--- intro_js/index.js
+++ intro_js/index.js
@@ -1,28 +1,176 @@
+//Processamento síncrono e assíncrono
+//Processamento bloqueante (síncrono)
+// console.log('Eu primeiro...')
+// console.log('Agora eu....')
+// console.log('Sempre serei a última...')
+
+// const a = 5 + 6
+// const b = 9 + 4
+// console.log(a + b) //linha 3 depende das linhas 1 e 2
+
+// function demorada(tempo){
+//     console.log('Comecei agora')
+//     const dataAtualMaisTempo = new Date().getTime() + tempo
+//     while(new Date().getTime() <= dataAtualMaisTempo);
+//     const d = 8 + 2 * 6
+//     console.log(`Demorada com tempo: ${tempo}`)
+//     return d
+// }
+
+// setTimeout(() => {
+//     demorada(2000)
+// }, 500 /*tempo de demora pra entrar na fila*/)
+
+// setTimeout(() => {
+//     demorada(1000)
+// },0) //Em geral as funções agendadas respeitam a ordem da fila de agendamento
+
+//CPU Bound: predominantemente caracterizada por ciclos de CPU, ex: While
+//IO Bound: predominantemente caracterizada por operações de Entrada e Saída, ex: Acesso a arquivos
+
+// const fs = require('fs') //realiza leitura de arquivos no Node js
+// const NomeArquivo = 'arquivo.txt'
+// //função callback (aquela que você define mas nunca chama)
+// const exibirConteudo = (erro, conteudo) => {
+//     console.log("A")
+//     if(erro){
+//         console.log(`Erro ao tentar abrir o arquivo... ${erro}`)
+
+//     }
+//     else{
+//         console.log(`Conteúdo: ${conteudo}`)
+//         const dobro = Number(conteudo.toString()) * 2
+//         //mais uma função callback
+//         const escrever = (erro) => {
+//             console.log(erro ? "Deu erro" : "Ok, escreveu o dobro")
+//             console.log('C')
+//             const exibirDobro = (erro, conteudo) => {
+//                 console.log('E')
+//                 console.log(erro? "Deu erro lendo o dobro" : `Dobro: ${conteudo.toString()}`)
+//             }
+//             fs.readFile('dobro.txt', exibirDobro)
+//             console.log('F')
+//         }
+//         fs.writeFile('dobro.txt', dobro.toString(), escrever)
+//         console.log('D')
+//     }
+// }
+// fs.readFile(NomeArquivo, exibirConteudo)
+// console.log("B")
+
+//promises
+//1 + 2 + 3 + ... + n
+//demorando...
+const calculoDemorado = (n) => {
+    let cont = 0
+    for(let i = 1; i<=n; i++){
+        cont += i 
+    }
+    return cont
+}
+const res = calculoDemorado(10000000000)
+console.log(res)
+
+
+// setTimeout(() => {
+//     console.log('Agendada pela setTimeout')
+// }, 0)
+// const dataAtualMais10Segundos = new Date().getTime() + 10000
+// while(new Date().getTime() <= dataAtualMais10Segundos);
+// console.log("Terminando o script principal...") //a função agendada jamais que termina antes que o script principal terminar, por mais que demore
+
+// const a = 2 + 3
+// const b = 6 * 1
+
+// setTimeout(() => {
+//     const d = demorada()
+//     console.log(`d: ${d}`)
+// }, 500) //agenda a execução de outra função
+
+// const e = a * b + 2
+// console.log(e)
+
+//Uma calculadora realiza as quatro operações fundamentais
+//soma: representada por uma arrow function que faz return
+//subtração: representada por uma arrow function sem return
+//multiplicação: function regular
+//divisão: você escolhe
+
+// const calculadora = {
+//     operacoes: {
+//         soma:  (a, b) => {
+//         return a + b
+//     },
+//         subtracao: (a, b) => a - b,
+
+//         multiplicacao: function (a,b){
+//         return a * b
+//     },
+//         divisao: (a, b) => {
+//         return a/b
+//     },
+//     }
+// }
+
+// console.log(calculadora.operacoes.soma(2,3))
+// console.log(calculadora.operacoes.subtracao(2,3))
+// console.log(calculadora.operacoes.multiplicacao(2,3))
+// console.log(calculadora.operacoes.divisao(2,3))
+
+//Uma concessionária tem nome, CNPJ e endereco (logradouro, numero e bairro). Ela também tem seu estoque de veículos. A quantidade de veículos é arbitrária. A qualquer instante, ela pode ter 2 ou 5 ou 17 veículos. Cada veículo tem modelo, marca e placa.
+
+// const concessionaria = {
+//     nome: 'Bla',
+//     CNPJ: 5266727,
+//     endereco:{
+//         logradouro: 'Rua C',
+//         numero: 50,
+//         bairro: {
+//             nome: 'Jardim',
+//         } 
+//     },
+//     estoque:[
+//         {
+//             modelo: 'Kicks',
+//             marca: 'Nissan',
+//             placa: 'ABC-1234',
+//         },
+//         {
+//             modelo: 'Fit',
+//             marca: 'Honda',
+//             placa: 'CDE-5678',
+//         }
+//     ]
+// }
+
+// console.log(concessionaria.estoque[0].modelo)
+// console.log(concessionaria['estoque'][1]['modelo'])
+
 //JSON: JavaScript Object Notation
 //Uma pessoa que se chama João e tem 17 anos
 //Uma pessoa se chama Maria, tem 21 anos e mora na Rua B, número 50
 // {} objeto js
-let pessoa = {
-    nome: 'João',
-    idade: 17,
-}
-console.log("Me chamo " + pessoa.nome);
-console.log("Tenho " + pessoa["idade"] + " anos")
+// let pessoa = {
+//     nome: 'João',
+//     idade: 17,
+// }
+// console.log("Me chamo " + pessoa.nome);
+// console.log("Tenho " + pessoa["idade"] + " anos")
 
-const pessoa2 = {
-    nome: 'Maria',
-    idade: 21,
-    endereco: {
-        logradouro: 'Rua B',
-        numero: 50,
-    },
-}
-console.log(pessoa2.endereco.logradouro)
-console.log(pessoa2['endereco']['numero'])
-console.log(pessoa2.endereco['logradouro'])
-console.log(pessoa2['endereco'].numero)
-console.log(pessoa2)
-console.log(pessoa2.endereco)
+// const pessoa2 = {
+//     nome: 'Maria',
+//     idade: 21,
+//     endereco: {
+//         logradouro: 'Rua B',
+//         numero: 50,
+//     },
+// }
+// console.log(pessoa2.endereco.logradouro)
+// console.log(pessoa2['endereco']['numero'])
+// console.log(pessoa2.endereco['logradouro'])
+// console.log(pessoa2['endereco'].numero)
+// console.log(pessoa2)
+// console.log(pessoa2.endereco)
 
 // const eAgora = () => {
 //     let cont = 1
